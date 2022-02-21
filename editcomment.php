0c678d5be97467d033a53c5f12c6102dbe63c37a
folakemi10 <f.shofu@wustl.edu>
2022-02-21 11:42:46 -0600

    css

--- editcomment.php
+++ editcomment.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Comment</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
 
-<?php
+    <?php
     require 'connectdatabase.php';
     session_start();
     //get story_id
     $username = $_SESSION['user_id'];
     $comment_id = $_POST['comment_id'];
 
-    if(!hash_equals($_SESSION['token'], $_POST['token'])){
+    if (!hash_equals($_SESSION['token'], $_POST['token'])) {
         die("Request forgery detected for edit story");
     }
 
@@ -33,23 +36,24 @@
     $stmt->bind_param('i', $comment_id);
     $stmt->execute();
 
-    $stmt->bind_result($comment_content);  
+    $stmt->bind_result($comment_content);
     $stmt->fetch();
     $stmt->close();
 
     echo "<h1> Edit Comment </h1>";
     echo "<form action=\"updatecomment.php\" method = \"POST\">
-            <input type=hidden name=\"token\" value=" . $_SESSION['token'] . ">
-            <input type=hidden name=\"comment_id\" id=\"comment_id\" value=\"" . $comment_id . "\"/>
-            <textarea name=\"edit_comment\" id = \"edit_comment\" rows=\"5\" cols=\"50\"> " . $comment_content . "</textarea>
+            <input class=create_story type=hidden name=\"token\" value=" . $_SESSION['token'] . ">
+            <input class=create_story type=hidden name=\"comment_id\" id=\"comment_id\" value=\"" . $comment_id . "\"/>
+            <textarea class=create_story name=\"edit_comment\" id = \"edit_comment\" rows=\"5\" cols=\"50\"> " . $comment_content . "</textarea>
             <input type=submit value = \"Update Comment\" />
         </form>";
-?>
+    ?>
 
-<form action="account.php">
-    <input type="submit" value="Back to My Account" />
-</form>
+    <form action="account.php">
+        <input type="submit" value="Back to My Account" />
+    </form>
 
-    
+
 </body>
+
 </html>
